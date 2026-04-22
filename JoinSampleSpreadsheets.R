aaced97b058bdb71dd66014cc67dcca082722237
smccollum98 <seanmccollum.98@gmail.com>
2026-04-21 17:58:23 -0700

    Made changes to improve function of code. - Date is retrieved from sample data filename - Class and name are retrieved from classification tracking sheet according to location and date. Have not yet implemented code to fill in blank "name" values in the collated sample dataframe.

--- JoinSampleSpreadsheets.R
+++ JoinSampleSpreadsheets.R
@@ -33,7 +33,11 @@ samples <- bind_rows(ldf, .id = "column_label")
 samples <- samples %>% 
   mutate(SiteCode = substr(.$filename, 1, 3)) %>% 
   mutate(orthomosaicdate = substr(.$filename, 6, 13)) %>% 
-  mutate(filedate = substr(.$filename, 15, 22))
+  mutate(filedate = substr(.$filename, 15, 22)) %>% 
+  mutate(year = substr(.$orthomosaicdate, 1, 4)) %>% 
+  mutate(month = substr(.$orthomosaicdate, 5, 6)) %>% 
+  mutate(day = substr(.$orthomosaicdate, 7, 8)) %>% 
+  mutate(date = paste0(year, "-", month, "-", day))
 
 ## Import classifiation tracking sheet
 trackingSheet <- read_xlsx(here("Classification_Tracking_Sheet.xlsx"), sheet = "TrackingData")
@@ -45,7 +49,9 @@ siteNames <- read_xlsx(here("Metadata", "SiteNamesMetadata.xlsx"))
 
 ### Add appropriate site values according to the site code ###
 
-Samples <- merge(samples, siteNames, by = "SiteCode")
+samples <- merge(samples, siteNames, by = "SiteCode")
+
+
 
 #### |||| #### |||| ####
 
@@ -54,12 +60,23 @@ Samples <- merge(samples, siteNames, by = "SiteCode")
 ## Need to transpose the "class" columns so that there is a "label" column and multiple rows with 1, 2, 3, etc. and a class name column...
 
 classNames <- trackingSheet %>% 
-  select(c("SiteCode", "Date8Digit", ))
+  select(c("Subarea", "Date8Digit", "Class0", "Class1", "Class2", "Class3", "Class4", "Class5")) %>% 
+  pivot_longer(c(Class0, Class1, Class2, Class3, Class4, Class5), names_to = "label", values_to = "name") %>% 
+  mutate(label = gsub("Class", "", label)) %>% 
+  mutate(label = as.integer(label))
+
+
+
+
+test <- merge(samples, classNames)
+
+
+#test <- pivot_longer(classNames, c(Class0, Class1, Class2, Class3, Class4, Class5), names_to = "Label", values_to = "Name")
 
 ### Create or fill in certain data columns ###
 
 ## For each dataframe, if there are no "Name" values, 
-  ##pull names from the classification tracking document according to the date and label
+##pull names from the classification tracking document according to the date and label
 
 
 ## Create a veg/unveg column. Populate it with "Vegetated" or "Bare" based on the translation established above.
