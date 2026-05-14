35208913def7cc732600f9a0b9d63ea4f9a96bdd
smccollum98 <seanmccollum.98@gmail.com>
2026-05-14 15:26:15 -0700

    Added a figure and pulling data to the classification raster analysis code.

--- ClassificationAnalysis.R
+++ ClassificationAnalysis.R
@@ -5,12 +5,16 @@ library(sf)
 library(stars)
 library(dplyr)
 library(here)
+library(ggplot2)
 
 grid <- st_read(here("AnalysisGrid", "HP1", "HP1_5m_grid.shp"))
 
-raster <- read_stars(here("ClassificationRasters", "MP1_191025_RF_241215_Mosaic.tif"))
+raster <- read_stars(here("ClassificationRasters", "MP1_241008_RF_Classification_241206_Mosaic.tif"))
 
-aggregate(raster, grid, mean)
+#aggregate(raster, grid, mean)
 
+ggplot()+
+  geom_stars(data = raster)+
+  geom_sf(data = grid, alpha = 0)
 
-
+raster_by_polygon <- aggregate(raster, grid, mean)
